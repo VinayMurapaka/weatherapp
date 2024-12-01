5525f44e4b23361b5de7b2cf898e4d6587b7336f
VinayMurapaka <161616358+VinayMurapaka@users.noreply.github.com>
2024-12-02 00:40:49 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,14 @@
+
+
+export const geoApiOptions= { 
+	method: 'GET',
+	headers: {
+		'x-RapidAPI-Key': 'esGbwrm390mshS2BCl0RAlxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+		'x-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b64704194403cbac8bce9a3715e9bced";
